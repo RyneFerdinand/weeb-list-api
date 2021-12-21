632f2d7b3aac37dd9d05482a3d2b856992dcc738
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2021-12-21 21:56:54 +0700

    update

--- server.js
+++ server.js
@@ -2,10 +2,11 @@ require('dotenv').config()
 
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 const animeRouter = require("./routes/anime");
 app.use('/anime', animeRouter);
 
-app.listen(3000, () => console.log("Server Started on Port 3000"))
+app.listen(PORT, () => console.log("Server Started on Port " + PORT))
