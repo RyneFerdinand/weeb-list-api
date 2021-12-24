5fb9cdea689e7567857187d858d2aaaa833fe27e
RyneFerdinand <73730502+RyneFerdinand@users.noreply.github.com>
2021-12-24 13:20:13 +0700

    test

--- routes/anime.js
+++ routes/anime.js
@@ -6,8 +6,21 @@ const router = express.Router()
 const API_URL = process.env.API_URL
 
 router.get('/:id', async (req, res)=>{
-    let URL = "https://weeb-list-api.herokuapp.com/anime/38671"
-    // let URL = `${API_URL}${req.baseUrl}/${req.params.id}`
+    let id = req.params.id
+    let URL = API_URL + "/anime/" + id;
+    try {
+        const anime = await axios.get(URL);
+        res.json(anime.data)
+    } catch (error) {
+        res.json(error.message)
+    }
+})
+
+router.get('/top/:page', async (req, res)=>{
+    let page = req.params.page
+    let URL = API_URL + "/top/anime/" + page
+
+    console.log(URL)
     try {
         const anime = await axios.get(URL);
         res.json(anime.data)
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 require('dotenv').config()
 
 const express = require("express");
+const cors = require('cors')
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+app.use(cors());
+
 
 const animeRouter = require("./routes/anime");
 app.use('/anime', animeRouter);
